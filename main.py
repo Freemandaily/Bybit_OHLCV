26b7b35389a5dab16cdde6cf013cbe4b9a31e5d3
freemandaily <onahchinemelem@gmail.com>
2025-07-16 01:35:22 +0100

    Bybit price remote helper Fetcher

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+import time,logging
+from fastapi import FastAPI
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+
+app  = FastAPI()
+
+@app.get("/bybit/ohlcv")
+def get_bybit_price_ohlcv(
+    symbol="BTCUSDT",
+    category="linear",
+    interval="15",       
+    start_time=None,
+    end_time=None,
+    limit=1000,
+    checkAlive: bool = False
+):
+    if checkAlive:
+        logging.info("Checking if Bybit API is alive...")
+        return {"status": "API is alive"}
+    
+    url = "https://api.bybit.com/v5/market/kline"
+    params = {
+        "symbol": symbol,
+        "category": category,
+        "interval": interval,
+        "start": start_time,
+        "end": end_time,
+        "limit": limit
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        return {"error": f"Failed to fetch data: {response.status_code}"}
+    data = response.json()
+    logging.info(f"Successfuflly Fetched Price For Symbol: {symbol} with Interval: {interval}")
+    return data
+    
